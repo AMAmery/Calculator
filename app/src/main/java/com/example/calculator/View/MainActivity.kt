3bd7042f7ac0d65fa9d4e4f4f786bd79701434b3
ali amery <limery20051384@gmail.com>
2023-05-26 07:28:33 -0930

    first commit

---
+++ app/src/main/java/com/example/calculator/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.calculator.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculator.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
